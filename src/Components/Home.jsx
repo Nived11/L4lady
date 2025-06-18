189cedfc2d800b2b9d0a663fd0440752d375aa3c
Nived <nivedsuresh5@gmail.com>
2025-06-18 22:10:16 +0530

    just started..

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ function Home(){
                 <h1 className="brand">L4Lady</h1>
                 <h2 className="soon">Coming Soon</h2>
                 <p className="follow">Follow us on Instagram</p>
-                <a href="https://instagram.com" target="_blank" className="insta-link">Instagram</a>
+                <a href="https://www.instagram.com/l4.lady?igsh=YjRqMjR5c3U3cGoz" target="_blank" className="insta-link">Instagram</a>
             </div>
         </div>
     )
